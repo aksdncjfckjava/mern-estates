d4e3f8da5977726081b3e2181163fbad71ea23e7
Abhishek Singh <abhishekdef@gmail.com>
2024-05-05 01:30:30 +0530

    Creates Pages and routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,31 @@
-import React from 'react'
+
+import  {BrowserRouter,Routes,Route} from 'react-router-dom'
+
+import Home from './Pages/Home'
+import About from './Pages/About'
+import Sigin from './Pages/Sigin'
+import Signup from './Pages/Signup'
+import Profile from './Pages/Profile'
 
 export default function App() {
   return (
-    <div>
+  
+    <BrowserRouter>
+    <Routes>
+     
+     <Route path="/"  element={<Home />}/>
+     <Route path="/Sigin" element={<Sigin  />}/>
+     <Route path="/Signup" element={<Signup  />}/>
+     <Route path="/About" element={<About />}/>
+     <Route path="/Profile" element={<Profile  />}/>
+     
 
-      <h1 className='text-red-500'> hello world</h1>
-      
-    </div>
+
+     
+     
+
+    </Routes>
+    
+    </BrowserRouter>
   )
 }
